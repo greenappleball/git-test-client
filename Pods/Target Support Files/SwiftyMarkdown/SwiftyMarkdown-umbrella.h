b5d659a8c54dcac26165be5554aaea4f7df1ed60
Dmitri Petrishin <dmitri.petrishin@postindustria.com>
2016-12-14 13:59:29 +0200

    #Down installed

--- Pods/Target Support Files/SwiftyMarkdown/SwiftyMarkdown-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SwiftyMarkdown.h"
-
-FOUNDATION_EXPORT double SwiftyMarkdownVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftyMarkdownVersionString[];
-
